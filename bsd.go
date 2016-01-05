11792940531443cbc8ab72d742e5f84c4068a7d2
Jack O'Connor <oconnor663@gmail.com>
2016-01-05 12:59:34 -0500

    make GetPasswd take a terminal file descriptor
    There are times when we need user input from the terminal, even though
    our stdin is piped from something else. (For example, reading an
    encrypted file over stdin, and asking the user for the password.) Make
    GetPasswd* take an explicit file descriptor for the terminal, so that
    the caller has the option of using /dev/tty.

--- bsd.go
+++ bsd.go
@@ -7,23 +7,23 @@ package gopass
 #include <unistd.h>
 #include <stdio.h>
 
-int getch() {
+int getch(int termDescriptor) {
         int ch;
         struct termios t_old, t_new;
 
-        tcgetattr(STDIN_FILENO, &t_old);
+        tcgetattr(termDescriptor, &t_old);
         t_new = t_old;
         t_new.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
+        tcsetattr(termDescriptor, TCSANOW, &t_new);
 
         ch = getchar();
 
-        tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
+        tcsetattr(termDescriptor, TCSANOW, &t_old);
         return ch;
 }
 */
 import "C"
 
-func getch() byte {
-	return byte(C.getch())
+func getch(termDescriptor int) byte {
+	return byte(C.getch(C.int(termDescriptor)))
 }
